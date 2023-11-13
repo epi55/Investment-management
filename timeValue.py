ab6f2bd49fdd76c95b067be62c588de0046d27f2
Robin <87436238+epi55@users.noreply.github.com>
2023-11-12 22:23:44 -0800

    Add files via upload

--- timeValue.py
+++ timeValue.py
@@ -1,36 +1,37 @@
-# Time Value Money (TVM)
-# "Compound interest" ("interest on interest") is important to TVM
-# Present Value (PV) and Future Value (FV)
-# Measuring PV and FV will help compare investment alternatives
 # DICTIONARY
 # N = Number of compounding periods
 # I/Y = Interest rate per compounding period
-# PV = Present value
-# FV = Future value
 # PMT = Annuity payments, or constant periodic cash flow
 
-# Effective annual rate (EAR)
-def effectiveAnnualRate():
+# EFFECTIVE ANNUAL RATE (EAR)
+# https://www.investopedia.com/terms/e/effectiveinterest.asp
+def effectiveAnnualRate(ar, cp, years, pr):
+    # Periodic rate (pr) = nominal interest rate / number of periods
+    # Number of periods = compounding periods per year * number of years
+    # ear = (1 + periodic rate) ** (number of periods) - 1
+    ear = (1 + pr) ** (cp * years) - 1
+    print("Effective annual rate: {}.".format(round(float(ear), 2)))
+    return(round(float(ear), 2))
 
-    annual_rate = float(input("What is the annual rate? Use decimals for percent. "))
-    m = int(input("# of compounding periods per year? "))
-    y = int(input("# of years invested at this rate? "))
-    periodic_rate = (annual_rate / m)
-    ear = (1 + periodic_rate) ** (m * y) - 1
-    print(round(float(ear), 2))
+# EFFECTIVE ANNUAL YIELD (EAY)
+def effectiveAnnualRate(ear):
+    # ALGORITHM
+    eay = ai * (1 + ear)
+    print("Effective annual yield: {}.".format(round(float(eay), 2)))
+    return(eay)
 
-    amount_invested = float(input("What is the amount invested at stated rate and timeline? "))
-    eay = amount_invested * (1 + ear)
-    print(round(float(eay), 2))
-    # Effective annual yield (EAY)
-
-def futureValue(presentValue, iy, n):
-    # FV = PV(1 + I/Y)^N
-    ## IY = rate of return per compounding period
-    ## N = total number of compounding periods
-    futureValue = presentValue * (1 + iy)**n
+# FUTURE VALUE (FV)
+def futureValue(pv, iy, n):
+    # fv = pv(1 + i/y)^N
+    futureValue = presentValue * (1 + iy) ** n
     print("Future value: {}.".format(futureValue))
 
-#RUN
-effectiveAnnualRate()
-futureValue(200, 0.10, 2)
+# RUN
+# (1) annual rate, (2) compounding periods, (3) years, (4) periodic rate, (5) amount invested
+effectiveAnnualRate(ar, cp, years, pr, ai)
+
+#
+effectiveAnnualYield(effectiveAnnualRate(ar, cp, years, pr, ai))
+
+# (1) present value, (2) rate of return per compounding period, (3) total number of compounding periods
+futureValue(pv, iy, n)
